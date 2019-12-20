d352bd019c605bd1276206b6f187caf0244c8969
creativethien <thien.tran@ucf.edu>
2019-12-20 10:28:46 -0500

    Create newsNav.php

---
+++ newsNav.php
@@ -0,0 +1,28 @@
+<!--News category nav-->
+<div class="container my-4">
+	<nav class="navbar navbar-toggleable-lg navbar-light bg-faded">
+		<a class="navbar-brand" href="http://localhost/fiea_02/news">News</a>
+		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/fiea_02/news/category/events">Events</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/fiea_02/news/category/admissions">Admissions</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/fiea_02/news/category/student-alumni">Student & Alumni</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/fiea_02/news/category/awards">Awards</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="http://localhost/fiea_02/news/category/industry-tech">Industry & Tech</a>
+				</li>
+			</ul>
+		</div>
+	</nav> 	
+</div>	  
