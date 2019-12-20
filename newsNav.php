1a1871cf8f99120652da06d5e6d34fe54a31626a
creativethien <thien.tran@ucf.edu>
2019-12-20 10:34:49 -0500

    Update newsNav.php

--- newsNav.php
+++ newsNav.php
@@ -1,26 +1,26 @@
 <!--News category nav-->
 <div class="container my-4">
 	<nav class="navbar navbar-toggleable-lg navbar-light bg-faded">
-		<a class="navbar-brand" href="http://localhost/fiea_02/news">News</a>
+		<a class="navbar-brand" href="https://fieacahcmsdev.smca.ucf.edu/news">News</a>
 		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link" href="http://localhost/fiea_02/news/category/events">Events</a>
+					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/events">Events</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="http://localhost/fiea_02/news/category/admissions">Admissions</a>
+					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/admissions">Admissions</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="http://localhost/fiea_02/news/category/student-alumni">Student & Alumni</a>
+					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/student-alumni">Student & Alumni</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="http://localhost/fiea_02/news/category/awards">Awards</a>
+					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/awards">Awards</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="http://localhost/fiea_02/news/category/industry-tech">Industry & Tech</a>
+					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/industry-tech">Industry & Tech</a>
 				</li>
 			</ul>
 		</div>
