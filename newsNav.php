69a79a128fcda740f255406bac5b9530b8bfdb3e
creativethien <thien.tran@ucf.edu>
2020-04-13 21:12:31 -0400

    Update newsNav.php

--- newsNav.php
+++ newsNav.php
@@ -1,26 +1,26 @@
 <!--News category nav-->
 <div class="container my-4">
 	<nav class="navbar navbar-toggleable-lg navbar-light bg-faded">
-		<a class="navbar-brand" href="https://fieacahcmsdev.smca.ucf.edu/news">News</a>
+		<a class="navbar-brand" href="https://fiea.ucf.edu/news">News</a>
 		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/events">Events</a>
+					<a class="nav-link" href="https://fiea.ucf.edu/news/category/events">Events</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/admissions">Admissions</a>
+					<a class="nav-link" href="https://fiea.ucf.edu/news/category/admissions">Admissions</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/student-alumni">Student & Alumni</a>
+					<a class="nav-link" href="https://fiea.ucf.edu/news/category/student-alumni">Student & Alumni</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/awards">Awards</a>
+					<a class="nav-link" href="https://fiea.ucf.edu/news/category/awards">Awards</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="https://fieacahcmsdev.smca.ucf.edu/news/category/industry-tech">Industry & Tech</a>
+					<a class="nav-link" href="https://fiea.ucf.edu/news/category/industry-tech">Industry & Tech</a>
 				</li>
 			</ul>
 		</div>
